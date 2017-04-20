5611fddcb06a831309684ba518344bfb03f260b4
Roman Coedo <romancoedo@gmail.com>
2017-04-20 09:14:53 +0200

    Support indexed inlets

--- src/inlet.js
+++ src/inlet.js
@@ -11,13 +11,13 @@ export class Inlet extends Component {
   componentWillReceiveProps(nextProps) {
     if (this.props.label !== nextProps.label) {
       removeInlet(this.context.registry, this.props.label, this.id);
-      addInlet(this.context.registry, nextProps.label, this.id);
+      addInlet(this.context.registry, nextProps.label, { id: this.id, index: this.props.index });
     }
     updateChildren(this.context.registry, this.id, nextProps.children);
   }
 
   componentWillMount() {
-    addInlet(this.context.registry, this.props.label, this.id);
+    addInlet(this.context.registry, this.props.label, { id: this.id, index: this.props.index });
     updateChildren(this.context.registry, this.id, this.props.children);
   }
 
@@ -34,8 +34,13 @@ Inlet.contextTypes = {
   registry: PropTypes.object.isRequired,
 };
 
-Inlet.PropTypes = {
+Inlet.propTypes = {
   label: PropTypes.string.isRequired,
+  index: PropTypes.number,
+};
+
+Inlet.defaultProps = {
+  index: 0,
 };
 
 export default Inlet;
