a5ef578558a8c4b008160a2a2b49d61704542adc
Roman Coedo <romancoedo@gmail.com>
2017-05-23 11:26:35 +0200

    Update to react 15.5

--- src/conduit-provider.js
+++ src/conduit-provider.js
@@ -1,4 +1,5 @@
-import React, { Component, Children, PropTypes } from "react";
+import React, { Component, Children } from "react";
+import PropTypes from "prop-types";
 import { createRegistry } from "./registry";
 
 class ConduitProvider extends Component {
