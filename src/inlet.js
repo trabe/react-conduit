6308854efd56fb3f8b111417f3e296e0b040a530
Roman Coedo <romancoedo@gmail.com>
2017-05-23 11:26:35 +0200

    Update to react 15.5

--- src/inlet.js
+++ src/inlet.js
@@ -1,4 +1,5 @@
-import React, { Component, Children, PropTypes } from "react";
+import React, { Component, Children } from "react";
+import PropTypes from "prop-types";
 import uuidV4 from "uuid/v4";
 import { updateChildren, removeInlet, addInlet } from "./registry";
 
