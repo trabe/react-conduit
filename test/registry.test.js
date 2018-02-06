971b0de8529c392b1325dc067430825ec0e3290e
David Barral <david@trabe.io>
2018-02-06 15:35:18 +0100

    Updated dependencies. Remove Chai and Sinon

--- test/registry.test.js
+++ test/registry.test.js
@@ -1,12 +1,6 @@
 import React from "react";
-import { mount } from "enzyme";
-import sinon from "sinon";
-import Chai from "chai";
-import ChaiSorted from "chai-sorted";
+import { mount, render } from "enzyme";
 import { ConduitProvider, Inlet, Outlet, Registry } from "../src";
-
-Chai.use(ChaiSorted);
-const expect = Chai.expect;
 
 const mountWithRegister = (element, registry) => mount(element, { context: { registry } });
 const mountWithProvider = element =>
@@ -23,23 +17,25 @@ describe("registry", () => {
     registry = new Registry();
   });
 
-  it.only("should merge all the children from matching inlets", () => {
-    const wrapper = mountWithProvider(
-      <div>
-        <Inlet label="1">
-          <div>c1</div>
-        </Inlet>
-        <Inlet label="1">
-          <div>c2</div>
-        </Inlet>
-        <Inlet label="1" index={-1}>
-          <div>c0</div>
-        </Inlet>
-        <Outlet label="1" />
-      </div>,
+  it("should merge all the children from matching inlets", () => {
+    const wrapper = render(
+      <ConduitProvider>
+        <div>
+          <Inlet label="1">
+            <div>c1</div>
+          </Inlet>
+          <Inlet label="1">
+            <div>c2</div>
+          </Inlet>
+          <Inlet label="1" index={-1}>
+            <div>c0</div>
+          </Inlet>
+          <Outlet label="1" />
+        </div>
+      </ConduitProvider>
     );
 
-    wrapper.find(Outlet).children().forEach((c, i) => expect(c.html()).to.equal(`<div>c${i}</div>`));
+    expect(wrapper).toMatchSnapshot();
   });
 
   it("should update outlet when inlets change their children", () => {
@@ -51,9 +47,9 @@ describe("registry", () => {
     );
     const outlet = mountWithRegister(<Outlet label="1" />, registry);
 
-    expect(outlet.html()).to.equal("<div><p>content</p></div>");
+    expect(outlet.html()).toEqual("<div><p>content</p></div>");
     inlet.setProps({ children: <p>new content</p> });
-    expect(outlet.html()).to.equal("<div><p>new content</p></div>");
+    expect(outlet.html()).toEqual("<div><p>new content</p></div>");
   });
 
   it("should update outlets when inlets change their labels", () => {
@@ -66,11 +62,11 @@ describe("registry", () => {
     const outlet1 = mountWithRegister(<Outlet label="1" />, registry);
     const outlet2 = mountWithRegister(<Outlet label="2" />, registry);
 
-    expect(outlet1.html()).to.equal("<div><p>content</p></div>");
-    expect(outlet2.html()).to.equal("<div></div>");
+    expect(outlet1.html()).toEqual("<div><p>content</p></div>");
+    expect(outlet2.html()).toEqual("<div></div>");
     inlet.setProps({ label: "2" });
-    expect(outlet1.html()).to.equal("<div></div>");
-    expect(outlet2.html()).to.equal("<div><p>content</p></div>");
+    expect(outlet1.html()).toEqual("<div></div>");
+    expect(outlet2.html()).toEqual("<div><p>content</p></div>");
   });
 
   it("should update outlet when it changes its label", () => {
@@ -88,9 +84,9 @@ describe("registry", () => {
     );
     const outlet = mountWithRegister(<Outlet label="1" />, registry);
 
-    expect(outlet.html()).to.equal("<div><p>content 1</p></div>");
+    expect(outlet.html()).toEqual("<div><p>content 1</p></div>");
     outlet.setProps({ label: "2" });
-    expect(outlet.html()).to.equal("<div><p>content 2</p></div>");
+    expect(outlet.html()).toEqual("<div><p>content 2</p></div>");
   });
 
   it("should update outlet when a new inlet is rendered", () => {
@@ -102,14 +98,14 @@ describe("registry", () => {
     );
     const outlet = mountWithRegister(<Outlet label="1" />, registry);
 
-    expect(outlet.html()).to.equal("<div><p>content 1</p></div>");
+    expect(outlet.html()).toEqual("<div><p>content 1</p></div>");
     mountWithRegister(
       <Inlet label="1">
         <p>content 2</p>
       </Inlet>,
       registry,
     );
-    expect(outlet.html()).to.equal("<div><p>content 1</p><p>content 2</p></div>");
+    expect(outlet.html()).toEqual("<div><p>content 1</p><p>content 2</p></div>");
   });
 
   it("should update outlet when an inlet is unmounted", () => {
@@ -127,14 +123,14 @@ describe("registry", () => {
     );
     const outlet = mountWithRegister(<Outlet label="1" />, registry);
 
-    expect(outlet.html()).to.equal("<div><p>content 1</p><p>content 2</p></div>");
+    expect(outlet.html()).toEqual("<div><p>content 1</p><p>content 2</p></div>");
     inlet2.unmount();
-    expect(outlet.html()).to.equal("<div><p>content 1</p></div>");
+    expect(outlet.html()).toEqual("<div><p>content 1</p></div>");
   });
 
   it("should invoke onDisconnect on both inlet and outlet when a conduit gets disconnected", () => {
-    const inletSpy = sinon.spy();
-    const outletSpy = sinon.spy();
+    const inletSpy = jest.fn();
+    const outletSpy = jest.fn();
 
     const inlet = mountWithRegister(
       <Inlet label="1" onDisconnect={inletSpy}>
@@ -144,15 +140,15 @@ describe("registry", () => {
     );
     const outlet = mountWithRegister(<Outlet label="1" onDisconnect={outletSpy} />, registry);
 
-    expect(outlet.html()).to.equal("<div><p>content</p></div>");
+    expect(outlet.html()).toEqual("<div><p>content</p></div>");
     inlet.unmount();
-    expect(inletSpy.calledOnce).to.equal(true);
-    expect(outletSpy.calledOnce).to.equal(true);
+    expect(inletSpy).toHaveBeenCalledTimes(1);
+    expect(outletSpy).toHaveBeenCalledTimes(1);
   });
 
   it("should invoke onConnect on both inlet and outlet when a new conduit is connected", () => {
-    const inletSpy = sinon.spy();
-    const outletSpy = sinon.spy();
+    const inletSpy = jest.fn();
+    const outletSpy = jest.fn();
 
     const inlet = mountWithRegister(
       <Inlet label="1" onConnect={inletSpy}>
@@ -162,7 +158,7 @@ describe("registry", () => {
     );
     const outlet = mountWithRegister(<Outlet label="1" onConnect={outletSpy} />, registry);
 
-    expect(inletSpy.calledOnce).to.equal(true);
-    expect(outletSpy.calledOnce).to.equal(true);
+    expect(inletSpy).toHaveBeenCalledTimes(1);
+    expect(outletSpy).toHaveBeenCalledTimes(1);
   });
 });
